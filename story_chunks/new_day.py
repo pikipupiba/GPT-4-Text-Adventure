c2355131febb7de9b189c1d4ddf14d1178489d78
pikipupiba <jjjsutter@gmail.com>
2023-07-27 17:18:48 -0500

    CURRENTLY WORKING

---
+++ story_chunks/new_day.py
@@ -0,0 +1,11 @@
+new_day = {}
+
+new_day.update({
+    "0":
+    "As the fog of a new day dissipates and the bell rings, children swarm outside the doors, onto the playground buzzing with youthful energy. Classmates scamper toward their favorite spots. A group congregates near the swing set, another scurries toward the sandbox, drawing lines and boundaries for their pretend kingdoms.\n\nSome stubborn kids are occupied trying to climb the tallest tree on the corner of the playground, a feat only accomplished by a handful of 5th grade legends. On the other side, solitary souls are found whispering to themselves or doodling with chalk on a blank concrete canvas.\n\nAs a new kid on this playground, you feel the exhilarating sensation of endless possibilities. Where do you go? Who do you talk to? The decision is yours—choose your next move."
+    })
+
+new_day.update({
+    "1":
+    ""
+    })
