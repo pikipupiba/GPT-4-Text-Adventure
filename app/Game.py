8eab646e5a7e4b15c8f20985b206d79e097da4e7
pikipupiba <jjjsutter@gmail.com>
2023-08-04 13:14:32 -0500

    When you don't finish any of your cycles

--- app/Game.py
+++ app/Game.py
@@ -148,62 +148,65 @@ class Game:
         self.history[-1][1] = ""
         self.raw_history[-1][1] = ""
         for chunk in response:
+            if len(chunk["choices"][0]["delta"]) == 0:
+                break
+
             # print(self.raw_history[-1][1])
             # See what model the api actually used. This is important for tracking tokens.
             real_model = chunk.get("model", model)
-            if len(chunk["choices"][0]["delta"]) != 0:
-                content = chunk["choices"][0]["delta"]["content"]
+            # if len(chunk["choices"][0]["delta"]) != 0:
+            content = chunk["choices"][0]["delta"]["content"]
 
-                # Add everything to raw history
-                self.raw_history[-1][1] += content
-                # Don't add chunks to the chatbot if they are part of a JSON object
-                # Continue until the JSON object is complete
-                if inside_json:
-                    json_string += content
+            # Add everything to raw history
+            self.raw_history[-1][1] += content
+            # Don't add chunks to the chatbot if they are part of a JSON object
+            # Continue until the JSON object is complete
+            if inside_json:
+                json_string += content
 
-                    try:
-                        new_json = json.loads(json_string)
-                        if "Stats_Schema" in new_json:
-                            self.stats = new_json
-                            self.history[-1][1] += " COMPLETE!!!\n\n"
-                        elif "Combat_Schema" in new_json:
-                            self.combat = new_json
-                            if self.combat["Combat_Schema"]["success"] == True:
-                                self.history[-1][1] += " SUCCESS!!!\n\n"
-                            else:
-                                self.history[-1][1] += " FAILURE!!!\n\n"
-                        logger.info(new_json)
-
-                        inside_json=False
-                        json_string=""
-                        found_json_schema=None
-                            
-                    except json.JSONDecodeError:
-                    
-                        if found_json_schema == None:
-                            if "Stats_Schema" in json_string:
-                                logger.info("Found Stats_Schema!")
-                                found_json_schema="Stats_Schema"
-                                self.history[-1][1] += " Calculating stats "
-                            elif "Combat_Schema" in json_string:
-                                logger.info("Found Combat_Schema!")
-                                found_json_schema="Combat_Schema"
-                                self.history[-1][1] += " Calculating combat "
+                try:
+                    new_json = json.loads(json_string)
+                    if "Stats_Schema" in new_json:
+                        self.stats = new_json
+                        self.history[-1][1] += " COMPLETE!!!\n\n"
+                    elif "Combat_Schema" in new_json:
+                        self.combat = new_json
+                        if self.combat["Combat_Schema"]["success"] == True:
+                            self.history[-1][1] += " SUCCESS!!!\n\n"
                         else:
-                            if len(json_string)%5 == 0:
-                                self.history[-1][1] += "-"
-                else:
-                    # Found the start of a JSON object
-                    if content == "{\"":
-                        inside_json=True
-                        json_string += content
-                        self.history[-1][1] += "\n\n---"
-                    # Send response chunks to the chatbot
+                            self.history[-1][1] += " FAILURE!!!\n\n"
+                    logger.info(new_json)
+
+                    inside_json=False
+                    json_string=""
+                    found_json_schema=None
+                        
+                except json.JSONDecodeError:
+                
+                    if found_json_schema == None:
+                        if "Stats_Schema" in json_string:
+                            logger.info("Found Stats_Schema!")
+                            found_json_schema="Stats_Schema"
+                            self.history[-1][1] += " Calculating stats "
+                        elif "Combat_Schema" in json_string:
+                            logger.info("Found Combat_Schema!")
+                            found_json_schema="Combat_Schema"
+                            self.history[-1][1] += " Calculating combat "
                     else:
-                        # print(content)
-                        self.history[-1][1] += content
-                    
-                yield self.history
+                        if len(json_string)%5 == 0:
+                            self.history[-1][1] += "-"
+            else:
+                # Found the start of a JSON object
+                if content == "{\"":
+                    inside_json=True
+                    json_string += content
+                    self.history[-1][1] += "\n\n---"
+                # Send response chunks to the chatbot
+                else:
+                    # print(content)
+                    self.history[-1][1] += content
+                
+            yield self.history
 
         # print(self.raw_history[-1][1])
         # Calculate streaming token usage
