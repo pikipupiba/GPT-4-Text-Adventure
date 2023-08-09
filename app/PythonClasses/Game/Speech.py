a1b7f8a04fd46d5aef0a787cc1f82284fe1ccc40
nothavelizard <djodonohue@gmail.com>
2023-08-09 16:23:48 -0500

    Building Speech Class

--- app/PythonClasses/Game/Speech.py
+++ app/PythonClasses/Game/Speech.py
@@ -1,5 +1,44 @@
 import boto3
+from botocore.config import Config
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
 
 class Speech:
-    def __init__(self) -> None:
+    
+    config = Config(
+        region_name='us-east-1',
+        signature_version='v4',
+        retries={
+            'max_attempts': 10,
+            'mode': 'standard'
+        }
+    )
+    
+    client = boto3.client('polly', config=config)
+    
+    def __init__(self, text: str) -> None:
         pass
+    
+    def get_ssml(self, text: str):
+        pass
+    
+    def get_speech_response(self, text: str, voice: str = 'Brian', type = 'ssml'):
+        if "<speak>" not in text:
+            text = f"<speak>{text}</speak>"
+        self.response = self.client.synthesize_speech(
+        Engine='standard',
+        LanguageCode='en-US',
+        OutputFormat='mp3',
+        VoiceId=voice,
+        Text=text,
+        TextType='ssml'
+        )
+    
+    def save_speech_response():
+        pass
+    
+    
