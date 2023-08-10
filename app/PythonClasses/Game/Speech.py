44f17d63c8bb058a53317e1cb84ff0567e02bd62
nothavelizard <djodonohue@gmail.com>
2023-08-09 19:09:24 -0500

    formatted Speech and LLM

--- app/PythonClasses/Game/Speech.py
+++ app/PythonClasses/Game/Speech.py
@@ -8,48 +8,47 @@ import subprocess
 from tempfile import gettempdir
 from PythonClasses.LLM import LLM
 
+
 class Speech:
-    
     config = Config(
-        region_name='us-east-1',
-        signature_version='v4',
-        retries={
-            'max_attempts': 10,
-            'mode': 'standard'
-        }
+        region_name="us-east-1",
+        signature_version="v4",
+        retries={"max_attempts": 10, "mode": "standard"},
     )
-    
-    client = boto3.client('polly', config=config)
-    
+
+    client = boto3.client("polly", config=config)
+
     def __init__(self, text: str) -> None:
         pass
-    
+
     def get_ssml(self, text: str):
         LLM.predict(model="gpt-4", system_message=text, raw_history=[])
         [
-            {"role": "system","content": "You convert the user message from plaintext to SSML.\n\nDo not respond stating what you are doing, simply do."},
-            {"role": "user","content": text},
+            {
+                "role": "system",
+                "content": "You convert the user message from plaintext to SSML.\n\nDo not respond stating what you are doing, simply do.",
+            },
+            {"role": "user", "content": text},
         ]
 
-
-    def get_speech_response(self, text: str, voice: str = 'Brian', type = 'ssml'):
+    def get_speech_response(self, text: str, voice: str = "Brian", type="ssml"):
         if "<speak>" not in text:
             text = f"<speak>{text}</speak>"
         self.response = self.client.synthesize_speech(
-        Engine='standard',
-        LanguageCode='en-US',
-        OutputFormat='mp3',
-        VoiceId=voice,
-        Text=text,
-        TextType='ssml'
+            Engine="standard",
+            LanguageCode="en-US",
+            OutputFormat="mp3",
+            VoiceId=voice,
+            Text=text,
+            TextType="ssml",
         )
-    
+
     def save_speech_response(self):
         if "AudioStream" in self.response:
-        # Note: Closing the stream is important because the service throttles on the
-        # number of parallel connections. Here we are using contextlib.closing to
-        # ensure the close method of the stream object will be called automatically
-        # at the end of the with statement's scope.
+            # Note: Closing the stream is important because the service throttles on the
+            # number of parallel connections. Here we are using contextlib.closing to
+            # ensure the close method of the stream object will be called automatically
+            # at the end of the with statement's scope.
             with closing(speech["AudioStream"]) as stream:
                 output = os.path.join(gettempdir(), "speech.mp3")
 
--- app/PythonClasses/LLM/LLM.py
+++ app/PythonClasses/LLM/LLM.py
@@ -15,75 +15,75 @@ import openai
 
 
 class LLM:
-    
-    def oneshot(system_message:str, user_message:str, model:str = "gpt-4"):
+    def oneshot(system_message: str, user_message: str, model: str = "gpt-4"):
         chat = openai.ChatCompletion.create(
             model=model,
-            messages= [{"role": "system", "content": system_message}, {"role": "user", "content": user_message}]
+            messages=[
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": user_message},
+            ],
         )
-        return chat.choices[0].message['content']
+        return chat.choices[0].message["content"]
 
     def build_openai_history_array(raw_history):
-
         logger.debug("Building history OpenAI format")
 
         if raw_history == None:
             logger.warning("No history provided. Returning None.")
             return None
-        
+
         history_openai_format = []
 
         # Convert history to OpenAI format
         for human, assistant in raw_history:
-            if human != None: history_openai_format.append({"role": "user", "content": human })
-            if assistant != None: history_openai_format.append({"role": "assistant", "content":assistant})
+            if human != None:
+                history_openai_format.append({"role": "user", "content": human})
+            if assistant != None:
+                history_openai_format.append(
+                    {"role": "assistant", "content": assistant}
+                )
 
-        logger.trace(f"Successfully built history OpenAI format | Found {len(history_openai_format)} turns")
+        logger.trace(
+            f"Successfully built history OpenAI format | Found {len(history_openai_format)} turns"
+        )
 
         return history_openai_format
 
-
     def build_openai_system_message(system_message: str = None, model: str = "gpt-4"):
-
         logger.debug("Building system message OpenAI format")
-
 
         if system_message == None:
             logger.warning("No system message provided. Returning None.")
             return None
-        
+
         if "gpt-4" in model:
-            system_message_openai_format = {
-                "role": "system",
-                "content": system_message
-            }
+            system_message_openai_format = {"role": "system", "content": system_message}
         else:
-            system_message_openai_format = {
-                "role": "user",
-                "content": system_message
-            }
+            system_message_openai_format = {"role": "user", "content": system_message}
 
         logger.trace("Successfully built system message OpenAI format")
 
         return system_message_openai_format
 
-
-    def predict(model: str = None, system_message: str = None, raw_history: List[any] = None):
-
+    def predict(
+        model: str = None, system_message: str = None, raw_history: List[any] = None
+    ):
         if model == None:
             logger.warning("No model provided. Returning [].")
             return []
-        
+
         if system_message == None:
             logger.warning("No system message provided. Returning [].")
             return []
-        
+
         if raw_history == None:
             logger.warning("No history provided. Returning [].")
             return []
 
-        logger.info(f"Predicting with model: {model} | User message: {raw_history[-1][0]}")
-        
+        logger.info(
+            f"Predicting with model: {model} | User message: {raw_history[-1][0]}"
+        )
+
         openai_system_message = LLM.build_openai_system_message(system_message, model)
         openai_history = LLM.build_openai_history_array(raw_history)
 
@@ -96,14 +96,10 @@ class LLM:
 
         # OpenAI API call
         return openai.ChatCompletion.create(
-            model=model,
-            messages= messages_openai_format,         
-            temperature=1.0,
-            stream=True
+            model=model, messages=messages_openai_format, temperature=1.0, stream=True
         )
-    
-    def summarize(self, text: str = None, model: str = "gpt-4"):
 
+    def summarize(self, text: str = None, model: str = "gpt-4"):
         if text == None:
             logger.warning("No text provided. Returning [].")
             return []
@@ -118,5 +114,5 @@ class LLM:
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
-            stop=["\n"]
+            stop=["\n"],
         )
