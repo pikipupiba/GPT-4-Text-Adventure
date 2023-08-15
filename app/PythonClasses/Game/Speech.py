6d77a2e49182b9a20d609a372061b0bb2c32a9ae
nothavelizard <djodonohue@gmail.com>
2023-08-15 18:43:10 -0500

    I think speech might work but no promises

--- app/PythonClasses/Game/Speech.py
+++ app/PythonClasses/Game/Speech.py
@@ -72,3 +72,52 @@ class LLMStreamProcessor:
         while any(terminator in self.buffer for terminator in self.terminators):
             sentence, self.buffer = self._split_at_first_terminator(self.buffer)
             await self._send_to_api(sentence)
+
+class LLMChunker:
+
+    AUDIO_FOLDER = os.path.join(os.getcwd(), "data", "audio")
+
+    def __init__(self, game_name: str):
+        self.config = Config(
+            region_name="us-east-1",
+            signature_version="v4",
+            retries={"max_attempts": 10, "mode": "standard"},
+        )
+        self.client = aioboto3.client("polly", config=self.config)
+        self.audio_path = os.path.join(LLMStreamProcessor.AUDIO_FOLDER, game_name)
+        
+        if not os.path.exists(self.audio_path):
+            os.makedirs(self.audio_path)
+
+    def _save_audio(self, audio_stream):
+        logger.info(f"Saving audio | {self.audio_path}")
+        with open(
+            os.path.join(
+                self.audio_path,
+                f"audio{len(os.listdir(self.audio_path))}.mp3"
+            ), "wb"
+        ) as file:
+            file.write(audio_stream.read())
+        return file.name
+
+    def _to_ssml(self, text):
+        system_message = """You convert the user message from plaintext to SSML.
+                    Do not respond stating what you are doing, simply do."""
+        return LLM.oneshot(system_message=system_message, user_message=text, model="gpt-3.5")
+
+    def _to_audio(self, text):
+        converted_text = self._to_ssml(text)
+        with self.client.synthesize_speech(
+            Engine="standard",
+            LanguageCode="en-US",
+            OutputFormat="mp3",
+            VoiceId="Brian",
+            Text=converted_text,
+            TextType="ssml",
+        ) as response:
+            if "AudioStream" in response:
+                with closing(response["AudioStream"]) as stream:
+                    self._save_audio(stream)
+
+    def speak(self, text):
+        return self._to_audio(text)
